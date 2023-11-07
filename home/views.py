1ff141cd46a5182bc0037cc49cfc048475e6f6dd
RahulMishra-29 <141310922+RahulMishra-29@users.noreply.github.com>
2023-11-07 16:01:46 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world. 96db8033 is the polls index.")
+    return HttpResponse("Hello, world. 372fd987 is the polls index.")
 # Create your views here.
